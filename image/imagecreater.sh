d089d492ed73e06b4390599447dc046ee5245e3c
mkg20001 <mkg20001@gmail.com>
2016-06-19 19:31:02 +0200

    Update Imagecreater

--- image/imagecreater.sh
+++ image/imagecreater.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Will Build the os-loader image
 
+set -e
+
 err() {
   echo "E: $1"
   exit $2
@@ -246,7 +248,7 @@ systemimage() {
   chinstall curl apt-transport-https
   chstd 'curl --silent https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
   VERSION=node_6.x
-  DISTRO=xenial
+  DISTRO='$dist'
   echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | tee /etc/apt/sources.list.d/nodesource.list
   echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | tee -a /etc/apt/sources.list.d/nodesource.list
   apt-get update'
@@ -301,5 +303,4 @@ cd $cur
 
 log "The image has been created sucessfully in $out/image.iso"
 log "The system-image has been created sucessfully in $out/system.img"
-cp $out/image.iso ~/IMG/test.iso
 exit 0
