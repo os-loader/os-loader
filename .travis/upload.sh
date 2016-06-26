67244891fe1d4066a13bc3a616732a474bc6732d
mkg20001 <mkg20001@gmail.com>
2016-06-26 15:02:28 +0200

    Update Travis

--- .travis/upload.sh
+++ .travis/upload.sh
@@ -1,7 +1,13 @@
 if [ -f /tmp/os-loader-builddir/IMAGE/output/image.iso ]; then
-  md5=`md5sum /tmp/os-loader-builddir/IMAGE/output/image.iso | fold -w 32 | head -n 1`
-  curl -X POST -F 'iso=@/tmp/os-loader-builddir/IMAGE/output/image.iso' -F "ap=$md5" https://mkg20001.sytes.net/os-loader/iso.php
-  echo "Uploaded as image.iso.$md5"
+  if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    md5=`md5sum /tmp/os-loader-builddir/IMAGE/output/image.iso | fold -w 32 | head -n 1`
+    commit=`git rev-parse HEAD`
+    ap="md5.$md5.commit.$commit.type.iso"
+    curl -X POST -F 'iso=@/tmp/os-loader-builddir/IMAGE/output/image.iso' -F "ap=$ap" https://mkg20001.sytes.net/os-loader/iso.php
+    echo "Uploaded as image.iso.$ap"
+  else
+    echo "Sucess - Skip Pull Upload"
+  fi
 else
   echo "Build was NOT SUCESSFULL - not uploading!"
 fi
