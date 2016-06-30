ae156e0968106a19ef920038e7a987cafe8191a0
mkg20001 <mkg20001@gmail.com>
2016-06-30 20:02:52 +0200

    Fix Uploads

--- .travis/upload.sh
+++ .travis/upload.sh
@@ -3,7 +3,7 @@ if [ -f /tmp/os-loader-builddir/IMAGE/output/image.iso ]; then
     md5=`md5sum /tmp/os-loader-builddir/IMAGE/output/image.iso | fold -w 32 | head -n 1`
     commit=`git rev-parse HEAD`
     ap="md5.$md5.commit.$commit.type.iso"
-    doupload="curl -X POST -F 'iso=@/tmp/os-loader-builddir/IMAGE/output/image.iso' -F \"ap=$ap\" https://mkg20001.sytes.net/os-loader/iso.php --connect-timeout 60 -m 300"
+    doupload="curl -X POST -F iso=@/tmp/os-loader-builddir/IMAGE/output/image.iso -F \"ap=$ap\" https://mkg20001.sytes.net/os-loader/iso.php --connect-timeout 60 -m 300"
     $doupload
     if [ $? -ne 0 ]; then
       echo "Upload FAIL! Retry..."
