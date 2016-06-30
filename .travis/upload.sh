029762322adadaa43d69496356b9ea2812f4c4d4
mkg20001 <mkg20001@gmail.com>
2016-06-30 19:45:11 +0200

    Fix Uploads

--- .travis/upload.sh
+++ .travis/upload.sh
@@ -1,16 +1,13 @@
-doupload() {
-  curl -X --connect-timeout 60 -m 300 POST -F 'iso=@/tmp/os-loader-builddir/IMAGE/output/image.iso' -F "ap=$ap" https://mkg20001.sytes.net/os-loader/iso.php
-  return $?
-}
 if [ -f /tmp/os-loader-builddir/IMAGE/output/image.iso ]; then
   if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     md5=`md5sum /tmp/os-loader-builddir/IMAGE/output/image.iso | fold -w 32 | head -n 1`
     commit=`git rev-parse HEAD`
     ap="md5.$md5.commit.$commit.type.iso"
-    doupload
+    doupload="curl -X POST -F 'iso=@/tmp/os-loader-builddir/IMAGE/output/image.iso' -F \"ap=$ap\" https://mkg20001.sytes.net/os-loader/iso.php --connect-timeout 60 -m 300"
+    $doupload
     if [ $? -ne 0 ]; then
       echo "Upload FAIL! Retry..."
-      doupload
+      $doupload
       if [ $? -ne 0 ]; then
         echo "Upload FAIL! Exit with 2"
         exit 2
