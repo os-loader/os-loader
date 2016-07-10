c392d086ddf747b01a6999a43200e4c83faa1191
mkg20001 <mkg20001@gmail.com>
2016-07-10 12:45:03 +0200

    Try to fix grub-install

--- gui/scripts/grub-install.sh
+++ gui/scripts/grub-install.sh
@@ -2,5 +2,10 @@
 
 progmax 1
 state "Install GRUB on $1"
-grub-install $1 --root-directory=$imagedir
+if [ -z $isos ]; then
+  rd="--root-directory=$imagedir"
+else
+  rd=""
+fi
+grub-install $1 $rd --boot-directory=$usb/boot/grub
 finish
