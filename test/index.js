6bb0c859aad4b88ce02afa1e5d45e5344d2f35d4
mkg20001 <mkg20001@gmail.com>
2016-07-16 09:18:17 +0200

    Start adding tests

---
+++ test/index.js
@@ -0,0 +1,16 @@
+assert = require('chai').assert;
+pth=require("path");
+
+function getPath() {
+  var a=[].slice.call(arguments,0);
+  a.unshift(__dirname,"..");
+  pth.join.apply(pth.join,a);
+}
+
+describe("gui",function() {
+  require(getPath("gui","test"));
+});
+
+describe("image",function() {
+  require(getPath("image","test"));
+});
