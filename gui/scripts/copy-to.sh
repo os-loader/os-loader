50fcf896760e969c265c048711f77eebb22929d6
mkg20001 <mkg20001@gmail.com>
2016-07-17 20:21:37 +0200

    Update path for system.img to filesystem.squashfs

--- gui/scripts/copy-to.sh
+++ gui/scripts/copy-to.sh
@@ -9,7 +9,7 @@ state "Copy Files to $dev"
 cp -r $from/boot $to/boot
 mkdir -p $to/boot/grub $to/os-loader/config
 prog 1
-cp -r $imagepath $to/os-loader/system.img
+cp -r $imagepath $to/os-loader/filesystem.squashfs
 
 rm -rf $imagedir/boot
 ln -s ../usb/boot $imagedir/boot
