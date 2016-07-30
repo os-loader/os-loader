aadcb987b3e537e41b8f39d152f3c30aed26f458
mkg20001 <mkg20001@gmail.com>
2016-07-30 14:13:21 +0200

    Update

--- server/core/acp/index.js
+++ server/core/acp/index.js
@@ -1,6 +1,24 @@
 //Admin Control Panel Part
+
+const b=require("core/backend");
+backend=new b(config);
+//global.backend=backend;
+
 var app=express.Router();
 app.use("/",function(req,res,next) {
   if (req.user) if (req.user.admin) return next(); else res.flash("error","Forbidden!").redirect("/"); else res.redirect("/Login");
+});
+app.use(function(req,res,next) {
+  var o=res.render.bind(res);
+  res.render=function(a,b,c) {
+    b.flash=res.getFlash();
+    b.user=req.user;
+    b.status=backend.status;
+    return o(a,b,c);
+  }
+  return next();
+});
+app.get("/",function(req,res) {
+  res.render("admin",{title:"Admin Home"});
 });
 module.exports=app;
---
+++ server/core/backend/index.js
@@ -0,0 +1,7 @@
+function backend(config) {
+  const self=this;
+  newLogger("backend",self);
+  self.info("Backend Starting...");
+  self.status={};
+}
+module.exports=backend;
