5c8b914e6dd14fa3cce1c1adf717a5589e82112d
mkg20001 <mkg20001@gmail.com>
2016-08-07 19:53:20 +0200

    Fix signing for gpg2

--- gui/deb-files/sign.sh
+++ gui/deb-files/sign.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+###
+#1: --utf8-strings
+#2: --textmode
+#3: --armor
+#4: --local-user
+#5: DEBFULLNAME
+#6: --clearsign
+#7: --output
+#8: FILE.dsc.asc
+#9: FILE.dsc
+###
+
+gpg2 $1 $3 $6 $9
