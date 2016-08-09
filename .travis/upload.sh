0df079c2d4eba374df5aba56f250b00562d40193
mkg20001 <mkg20001@gmail.com>
2016-08-09 12:09:29 +0200

    Change upload timeouts

--- .travis/upload.sh
+++ .travis/upload.sh
@@ -17,24 +17,17 @@ if [ -f /tmp/os-loader-builddir/IMAGE/output/image.iso ]; then
     md5=`md5sum /tmp/os-loader-builddir/IMAGE/output/image.iso | fold -w 32 | head -n 1`
     commit=`git rev-parse HEAD`
     ap="md5.$md5.commit.$commit.type.iso"
-    doupload="curl -X POST -F iso=@/tmp/os-loader-builddir/IMAGE/output/image.iso -F key=$UPLOADKEY -F ap=$ap https://mkg20001.sytes.net/os-loader/iso.php --connect-timeout 60 -m 1200"
-    $doupload
-    if [ $? -ne 0 ]; then
-      echo "Upload FAIL! Retry (1 of 3)..."
+    doupload="curl -X POST -F iso=@/tmp/os-loader-builddir/IMAGE/output/image.iso -F key=$UPLOADKEY -F ap=$ap https://mkg20001.sytes.net/os-loader/iso.php --connect-timeout 10 -m 300"
+    i=0;
+    doupload_exec() {
       $doupload
       if [ $? -ne 0 ]; then
-        echo "Upload FAIL! Retry (2 of 3)..."
-        $doupload
-        if [ $? -ne 0 ]; then
-          echo "Upload FAIL! Retry (3 of 3)..."
-          $doupload
-          if [ $? -ne 0 ]; then
-            echo "Upload FAIL! Exit with 2"
-            exit 2
-          fi
-        fi
+        let i=$i+1;
+        echo "Upload FAIL! Retry $i..."
+        doupload_exec
       fi
-    fi
+    }
+    doupload_exec
     echo "Uploaded as image.iso.$ap"
   else
     echo "Sucess - Skip Pull Upload"
