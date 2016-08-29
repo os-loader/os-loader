bd9320ca63f23184128e89246dd2105c96134402
mkg20001 <mkg20001@gmail.com>
2016-08-29 14:17:17 +0200

    Add background image

--- gui/scripts/grub-update.sh
+++ gui/scripts/grub-update.sh
@@ -9,6 +9,11 @@ state "Generate Config Files..."
 #script update-grub -o "$usb/boot/grub/grub.cfg"
 chroot grub-mkdevicemap
 chroot update-grub #-o "/usb/boot/grub.cfg"
+mkdir -p $usb/grub
 echo "configfile /boot/grub.cfg
 " > $usb/grub.cfg
+echo "configfile /boot/grub.cfg
+" > $usb/grub/grub.cfg
+echo "configfile /boot/grub.cfg
+" > $usb/boot/grub.cfg
 finish
