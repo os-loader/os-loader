b55462043d96e5d29a5100738e1049d1c5ac45f7
mkg20001 <mkg20001@gmail.com>
2016-09-01 13:25:22 +0200

    Remove postinstall scripts from server during gulp minify

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -34,7 +34,12 @@ gulp.task("index.js",function() {
 });
 
 gulp.task("package.json",function() {
-  return gulp.src("package.json").pipe(gulp.dest(dist()));
+  return gulp.src("package.json").pipe($.jsonTransform(function(data) {
+    delete data.devDependencies;
+    delete data.scripts.install;
+    delete data.scripts.postinstall;
+    return data;
+  })).pipe(gulp.dest(dist()));
 });
 
 gulp.task("views",function() {
